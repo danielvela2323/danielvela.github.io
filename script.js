9d3ac3baf2cf568c2e8ad6fe1dc2cb53f91eadd5
danielvela2323 <danieltecanvela88@gmail.com>
2026-03-03 17:23:34 -0600

    Add files via upload
    Second

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+const cursorLight = document.querySelector('.cursor-light');
+
+document.addEventListener('mousemove', (e) => {
+    cursorLight.style.left = e.clientX + 'px';
+    cursorLight.style.top = e.clientY + 'px';
+});
+
+
+// Para cuando el curso salga y cuando entre
+document.addEventListener('mouseleave', () => {
+    cursorLight.style.opacity = '0';
+});
+
+document.addEventListener('mouseenter', () => {
+    cursorLight.style.opacity = '1';
+});
+
 
 
 
